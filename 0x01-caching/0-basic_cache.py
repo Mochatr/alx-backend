9c7661a37e1cf17df76c39f182d1db67820bc5e6
Mochatr <mohamedchatr8@gmail.com>
2024-05-26 13:32:51 +0000

    fix: ensure the class BasicCache correctly inherits from BaseCaching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,39 +2,7 @@
 """
 Module that defines a basic cache system.
 """
-
-
-class BaseCaching:
-    """
-    BaseCaching defines the structure of a catching system
-    including the maximum number of items it can hold and
-    provides a template for storing data in a dictionary.
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """
-        Initializes the cache data storage
-        as an empty dictionary.
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """
-        Prints all key-value pairs in the cache
-        in an ascending order based on keys.
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """Add an item in the cache"""
-        raise Exception("Method not implemented")
-
-    def get(self, key):
-        """Get an item by key"""
-        raise Exception("Method not implemented")
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,35 +2,7 @@
 """
 Module that defines a FIFO cache system
 """
-
-
-class BaseCaching:
-    """
-    BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """
-        Initialize
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """
-        Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """
-        Add an item in the cache
-        """
-        raise Exception("Method not implemented")
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
